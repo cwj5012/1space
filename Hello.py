385602960d6a75662c7ef86b0c3efbe0d52035ea
cwj5012 <cwj5012@gmail.com>
2023-10-16 07:25:19 +0000

    1

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# 3 Welcome to Streamlit! 👋")
+    st.write("# Welcome to Streamlit!")
 
     st.sidebar.success("Select a demo above.")
 
